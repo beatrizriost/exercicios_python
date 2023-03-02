f65d763af42a4ab3a638f9515c77a2c15e1b62e5
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2023-03-02 10:00:50 -0300

    Create exercicio_10.py

---
+++ exercicio_10.py
@@ -0,0 +1,9 @@
+# Entrada
+sal_func = float(input(f'\n\tInsira os valores:\n\tSalário Funcionário: '))
+sal_min = float(input(f'\n\tSalário Mínimo: '))
+
+# Processamento
+qnt_sal = sal_func / sal_min
+
+# Saída
+print(f'\n\tEsse funcionário recebe {qnt_sal} salário(s) minímo(s)')
