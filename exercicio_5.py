dc87044821b44db951a6d8aa2b50ee48976ab4b7
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2023-03-02 09:55:51 -0300

    Create exercicio_5.py

---
+++ exercicio_5.py
@@ -0,0 +1,9 @@
+# Entrada
+alt = float(input(f'\n\tInsira os valores:\n\tAltura: '))
+raio = float(input(f'\n\tRaio: '))
+
+# Processamento
+vol = 3.14159 * raio * raio * alt
+
+# Saída
+print(f'\n\tVolume: {vol}')
