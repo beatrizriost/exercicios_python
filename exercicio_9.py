22a79ce7c014caca9e026ef8741bb4948d241997
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2023-03-02 10:00:11 -0300

    Create exercicio_9.py

---
+++ exercicio_9.py
@@ -0,0 +1,26 @@
+#Entrada
+num = int(input(f'\n\tInsira um número:'))
+
+#Processamento
+num1 = num * 1
+num2 = num * 2
+num3 = num * 3
+num4 = num * 4
+num5 = num * 5
+num6 = num * 6
+num7 = num * 7
+num8 = num * 8
+num9 = num * 9
+num10 = num * 10
+
+#Saída
+print(f'\n\t{num} x 1 = {num1}')
+print(f'\n\t{num} x 2 = {num2}')
+print(f'\n\t{num} x 3 = {num3}')
+print(f'\n\t{num} x 4 = {num4}')
+print(f'\n\t{num} x 5 = {num5}')
+print(f'\n\t{num} x 6 = {num6}')
+print(f'\n\t{num} x 7 = {num7}')
+print(f'\n\t{num} x 8 = {num8}')
+print(f'\n\t{num} x 9 = {num9}')
+print(f'\n\t{num} x 10 = {num10}')
