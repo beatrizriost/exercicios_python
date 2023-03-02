c5f3250e370f1b0fce4cae408a5af7afa75decfb
Beatriz Rios <110923126+beatrizriost@users.noreply.github.com>
2023-03-02 10:01:39 -0300

    Create exercicio_11.py

---
+++ exercicio_11.py
@@ -0,0 +1,9 @@
+# Entrada
+num = int(input(f'\n\tInsira um número\n\tNúmero:'))
+
+# Processamento
+raiz = num**0.5
+pot = num**5
+
+# Saída
+print(f'\n\tRaiz Quadrada: {raiz}\n\tElevado à quinta potência: {pot}')
